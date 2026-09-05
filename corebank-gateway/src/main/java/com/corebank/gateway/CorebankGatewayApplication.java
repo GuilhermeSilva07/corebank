00a0d847a8356683002648a953dd86468c6f67b9
Guilherme Silva <gs2281172@gmail.com>
2026-09-05 15:42:45 -0300

    chore: initial multi-module structure (corebank-parent + corebank-gateway) and local docker-compose

---
+++ corebank-gateway/src/main/java/com/corebank/gateway/CorebankGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.corebank.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebankGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorebankGatewayApplication.class, args);
+	}
+
+}
---
+++ corebank-gateway/src/test/java/com/corebank/gateway/CorebankGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corebank.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebankGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
